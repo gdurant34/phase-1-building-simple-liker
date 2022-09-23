0b5503cfa1c0ebfa4e98326e1d706173942fab60
Grace Durant <gdurant34@gmail.com>
2022-09-22 20:35:14 -0700

    Completed deliverables

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
-// Defining text characters for the empty and full hearts for you to use later.
+
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const likeHearts = document.querySelectorAll('.like-glyph');
+const errorModal = document.querySelector('#modal');
+errorModal.className = "hidden";
 
+likeHearts.forEach(heart => heart.addEventListener('click', callback));
 
+function callback(e) {
+  mimicServerCall()
+  .then(data => { // this runs when it's sucessful
+    const heartSpan = e.target;
+    let heartState = heartSpan.textContent; 
+  
+    if(heartState == EMPTY_HEART) {
+      e.target.textContent = FULL_HEART;
+      e.target.className = "activated-heart";
+    } else {
+      e.target.textContent = EMPTY_HEART;
+      e.target.className = "";
+    };
+  })
+  .catch(error => {
+    errorModal.className = "";
+    errorModal.innerText = error;
+    setTimeout(() => {errorModal.className = "hidden"}, 3000);
 
+  })
+  
+}
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
@@ -23,3 +48,11 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
+
+
+
+
+// locate the elements i want to add click events to
+// <span class="like-glyph">♡</span> - the heart 
